4d9373ba8c571a6871ec11e6227790140158547e
isadoragds <105572355+isadoragds@users.noreply.github.com>
2022-06-06 19:53:38 -0300

    Add files via upload
    Criando a tela de consulta e cadastro de referencias de lancamentos

---
+++ LancamentoCaixa/src/main/java/br/com/novatendencia/LancamentoaixCa/LancamentoaixCaApplication.java
@@ -0,0 +1,13 @@
+package br.com.novatendencia.LancamentoaixCa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LancamentoaixCaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LancamentoaixCaApplication.class, args);
+	}
+
+}
---
+++ LancamentoCaixa/src/test/java/br/com/novatendencia/LancamentoaixCa/LancamentoaixCaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.novatendencia.LancamentoaixCa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LancamentoaixCaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
